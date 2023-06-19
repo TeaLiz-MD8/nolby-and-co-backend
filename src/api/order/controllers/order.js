3f431bee3eaa07aeda50e09434dc8314a870b4e9
Лиза Макарова <lizamakarova@MacBook-Air-Liza.local>
2023-06-19 08:40:33 +0300

    initial commit

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -33,11 +33,11 @@ module.exports = createCoreController("api::order.order", ({ strapi }) => ({
       );
 
       const session = await stripe.checkout.sessions.create({
-        shipping_address_collection: { allowed_countries: ["IN"] },
+        shipping_address_collection: { allowed_countries: ["RU"] },
         payment_method_types: ["card"],
         mode: "payment",
         success_url: process.env.CLIENT_URL + `/success`,
-        cancel_url: process.env.CLIENT_URL + "/failed",
+        cancel_url: process.env.CLIENT_URL + `/failed`,
         line_items: lineItems,
       });
 
